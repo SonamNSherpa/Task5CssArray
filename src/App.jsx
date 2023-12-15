6e94d8b084ee676ea7ce8452c3a7514a6dca0c65
SonamNSherpa <Sonamsherpa16@gmail.com>
2023-12-15 15:15:47 -0500

    base code added

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import styles from "./App.module.css";
+
+const names = ["Alice", "Bob", "Mike", "John", "David"];
+const address = ["NY", "Sydney", "Ktm", "Florida", "Texas"];
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+
+    </>
+  );
+}
+
+export default App;
